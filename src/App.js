058ab96616b9a8345054cd231fbd5b5e8f3fb89e
Tiago Matos <tiagovolta@gmail.com>
2020-10-22 23:41:02 -0300

    Fix

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [alimentos, setAlimentos] = useState([]);
+  const [primeiroAlimento, setAlimento] = useState({});
+  const [primeiroAlimento2, setAlimento2] = useState({});
+
+  useEffect(() => {
+    axios.get(`https://taco-food-api.herokuapp.com/api/v1/food`)
+    .then(res => {
+      setAlimentos(res.data);
+    })
+  }, [])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="DivAlimentos">
+        <div className="Select">
+          {alimentos.map((a) => <div className={primeiroAlimento.id && a.id == primeiroAlimento.id?'divLine lineActive':'divLine'} onClick={() => setAlimento(a)}>{a.description}</div>)}
+        </div>
+        <div className="DivKcal">
+          <div className="DivTitle">Calorias</div>
+          {primeiroAlimento.attributes?(
+            <div>{primeiroAlimento.attributes.energy.kcal.toFixed(2)}</div>
+          ):(
+            <div>0</div>
+          )}
+        </div>
+      </div>
+      
+      <div className="DivAlimentos">
+        <div className="Select">
+          {alimentos.map((a) => <div className={primeiroAlimento2.id && a.id == primeiroAlimento2.id?'divLine lineActive':'divLine'} onClick={() => setAlimento2(a)}>{a.description}</div>)}
+        </div>
+        <div className="DivKcal">
+          <div className="DivTitle">Calorias</div>
+          {primeiroAlimento2.attributes?(
+            <div>{primeiroAlimento2.attributes.energy.kcal.toFixed(2)}</div>
+          ):(
+            <div>0</div>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
